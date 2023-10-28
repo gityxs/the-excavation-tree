81f03acde19ae5c1d978aed807fdcea8a4d67c3b
SSansssssssssssss <99086651+SSansssssssssssss@users.noreply.github.com>
2023-10-28 17:17:32 +0800

    Change endgame and add endgame text
    Change endgame and add endgame text

--- js/mod.js
+++ js/mod.js
@@ -73,11 +73,12 @@ function addedPlayerData() { return {
 
 // Display extra things at the top of the page
 var displayThings = [
+	"Current Endgame: 100M points"
 ]
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+	return player.points.gte(new Decimal("e8"))
 }
 
 
