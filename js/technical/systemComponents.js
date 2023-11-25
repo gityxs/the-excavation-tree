61ce431ed05dadc027a55aa3488f96562883e71b
SSansssssssssssss <99086651+SSansssssssssssss@users.noreply.github.com>
2023-11-25 20:21:31 +0800

    V1.2
    V1.2

--- js/technical/systemComponents.js
+++ js/technical/systemComponents.js
@@ -170,7 +170,10 @@ var systemComponents = {
                 <td><button class="opt" onclick="toggleOpt('hideChallenges')">Completed Challenges: {{ options.hideChallenges?"HIDDEN":"SHOWN" }}</button></td>
                 <td><button class="opt" onclick="toggleOpt('forceOneTab'); needsCanvasUpdate = true">Single-Tab Mode: {{ options.forceOneTab?"ALWAYS":"AUTO" }}</button></td>
 				<td><button class="opt" onclick="toggleOpt('forceTooltips'); needsCanvasUpdate = true">Shift-Click to Toggle Tooltips: {{ options.forceTooltips?"ON":"OFF" }}</button></td>
-				</tr> 
+		    </tr> 
+		    <tr>
+                <td><button class="opt" onclick="fixbattery()">Fix Battery Upgrades</button></td>
+            </tr>
         </table>`
     },
 
